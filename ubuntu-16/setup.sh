3628402e08f67d1a53c9eb0bd4b663d65c65ca99
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2017-09-11 15:33:43 +0300

    Create setup.sh

---
+++ ubuntu-16/setup.sh
@@ -0,0 +1,11 @@
+sudo add-apt-repository ppa:deluge-team/ppa
+sudo apt-get update
+sudo apt-get install deluge-webui deluged -y
+sudo adduser --system --gecos "Deluge Service" --disabled-password --group --home /var/lib/deluge deluge
+wget https://raw.githubusercontent.com/Tech-Blog/Deluge/master/ubuntu-16/deluged.service -O /etc/systemd/system/deluged.service
+wget https://raw.githubusercontent.com/Tech-Blog/Deluge/master/ubuntu-16/deluged.service -O /etc/systemd/system/deluge-web.service
+systemctl start deluged
+systemctl enable deluged
+systemctl start deluge-web
+systemctl enable deluge-web
+echo "your-server-ip:8112"

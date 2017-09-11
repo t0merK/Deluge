105904016c8dca47d341beb8065547ef3968ffde
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2017-09-11 16:08:45 +0300

    Update setup.sh

--- ubuntu-16/setup.sh
+++ ubuntu-16/setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install deluge-webui deluged -y
 sudo adduser --system --gecos "Deluge Service" --disabled-password --group --home /var/lib/deluge deluge
 wget https://raw.githubusercontent.com/Tech-Blog/Deluge/master/ubuntu-16/deluged.service -O /etc/systemd/system/deluged.service
-wget https://raw.githubusercontent.com/Tech-Blog/Deluge/master/ubuntu-16/deluged.service -O /etc/systemd/system/deluge-web.service
+wget https://raw.githubusercontent.com/Tech-Blog/Deluge/master/ubuntu-16/deluge-web.service -O /etc/systemd/system/deluge-web.service
 systemctl start deluged
 systemctl enable deluged
 systemctl start deluge-web
